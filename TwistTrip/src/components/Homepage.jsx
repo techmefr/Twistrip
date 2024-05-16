6211491f9968c06b50af1284f824ab0223c12d39
Maksim Losjuk <m.lysuk@gmail.com>
2024-05-16 13:59:42 +0200

    added hero video and base structure in css

---
+++ TwistTrip/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className=''>Hello</h1>
+      <h2>good day</h2>
+      <p>text</p>
+    </div>
+  )
+}
