a469f8ad9d040ca4c591345c0bcf762c8b0dc23d
romain_delol <delol.romain@gmail.com>
2024-05-16 14:08:43 +0200

    merge

--- TwistTrip/src/App.jsx
+++ TwistTrip/src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react';
 import "./App.css";
-import Homepage from "./components/HomePage";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
--- TwistTrip/src/components/Homepage.jsx
+++ TwistTrip/src/components/Homepage.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 export default function HomePage() {
   return (
     <div>
-      <h1 className=''>Hello</h1>
+      <h1 className="">Hello</h1>
       <h2>good day</h2>
       <p>text</p>
     </div>
-  )
+  );
 }
