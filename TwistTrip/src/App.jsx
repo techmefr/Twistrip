00ce771cfe1a9f3ff8ffb012e27493679ee6aa98
romain_delol <delol.romain@gmail.com>
2024-05-16 15:38:19 +0200

    modif

--- TwistTrip/src/App.jsx
+++ TwistTrip/src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import "./App.css";
 import Homepage from "./components/Homepage";
 
--- TwistTrip/src/components/Homepage.jsx
+++ TwistTrip/src/components/Homepage.jsx
@@ -1,6 +1,9 @@
+import logo from "../assets/logo/logoTwistTrip.png";
+
 export default function HomePage() {
   return (
     <div>
+      <img src={logo} alt="Logo" />
       <h1 className="">Hello</h1>
       <h2>good day</h2>
       <p>text</p>
