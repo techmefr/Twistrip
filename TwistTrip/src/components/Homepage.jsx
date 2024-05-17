4cab814769f17b18d222a2dcb52bedfd67ee7107
techmefr <compigni.gaetan@gmail.com>
2024-05-17 10:07:21 +0200

    add images to prize

--- TwistTrip/src/components/Homepage.jsx
+++ TwistTrip/src/components/Homepage.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import HeroVideo from '../assets/Images/Hero_video.mp4';
-import logo from '../assets/logo/logoTwistTrip.png';
-import FlashAnimation from './FlashAnimation';
+import HeroVideo from "../assets/images/Hero_video.mp4";
+import logo from "../assets/logo/logoTwistTrip.png";
+import FlashAnimation from "./FlashAnimation";
 
 export default function HomePage() {
-
   return (
     <>
       <div className="hero-container">
         <img src={logo} className="siteLogo" alt="siteLogo" />
         <video className="heroVideo" src={HeroVideo} autoPlay muted loop />
         <h1 className="paragraphe">Transformez vos vacances en aventure !</h1>
-        <p className='heroParagraph'>Payez, partez, regrettez...</p>
-        <FlashAnimation className='flash' />
+        <p className="heroParagraph">Payez, partez, regrettez...</p>
+        <FlashAnimation className="flash" />
       </div>
     </>
   );
